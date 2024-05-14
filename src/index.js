217f40188b24cee135ce1eb4003c58b371f9506d
Yevhenii Airapetian <sonkozhenia11@gmail.com>
2024-05-15 00:34:26 +0200

    feat: update repo

--- src/index.js
+++ src/index.js
@@ -1,14 +1,11 @@
-
-
-import { BrowserRouter } from 'react-router-dom';
 import React from 'react';
-import ReactDOM from 'react-dom';
+import ReactDOM from 'react-dom/client';
+import { BrowserRouter } from 'react-router-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-
 root.render(
   <BrowserRouter>
     <App />

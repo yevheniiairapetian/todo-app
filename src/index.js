04aff3b1c4dcbd6786d8afebd7431de0e9bd3978
Yevhenii Airapetian <sonkozhenia11@gmail.com>
2024-05-14 23:25:14 +0200

    feat: update app

--- src/index.js
+++ src/index.js
@@ -1,12 +1,14 @@
 import React from 'react';
-import * as ReactDOM from 'react-dom';
+
 import { BrowserRouter } from 'react-router-dom';
-import { createRoot } from 'react-dom';
+import React from 'react';
+import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
-const root = createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById('root'));
+
 root.render(
   <BrowserRouter>
     <App />

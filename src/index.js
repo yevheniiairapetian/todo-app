ae70156a4c9bdfab1c4049530ce87e869417e352
Yevhenii Airapetian <sonkozhenia11@gmail.com>
2024-05-15 00:59:04 +0200

    feat: change router to hash router

--- src/index.js
+++ src/index.js
@@ -3,13 +3,15 @@ import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
 import './index.css';
 import App from './App';
+import { HashRouter } from 'react-router-dom';
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <BrowserRouter>
+  
+    <HashRouter>
     <App />
-  </BrowserRouter>
+    </HashRouter>
 );
 
 // If you want to start measuring performance in your app, pass a function

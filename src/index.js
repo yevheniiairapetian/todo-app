fb37fd0cf6368502a02d338f207d0423fb04a441
Yevhenii Airapetian <sonkozhenia11@gmail.com>
2024-05-14 23:30:58 +0200

    fix: fix import statement

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@
 
 import { BrowserRouter } from 'react-router-dom';
 import React from 'react';
-import ReactDOM from 'react-dom/client';
+import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';

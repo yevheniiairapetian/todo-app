4cef0008636b0b2ca9a355078184c151f706391c
Yevhenii Airapetian <sonkozhenia11@gmail.com>
2024-05-14 23:01:42 +0200

    feat: update import statements

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import * as ReactDOM from 'react-dom';
 import { BrowserRouter } from 'react-router-dom';
-import { createRoot } from 'react-dom/client';
+import { createRoot } from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
